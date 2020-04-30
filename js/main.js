b60832f5c2d87961980b3c06833508e93f1841e4
Elena Angelova <eleangelo0717@gmail.com>
2020-04-30 19:05:33 +0700

    Корректровка цвета шрифта в футоре

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-let recordButton = document.querySelector('#record-button');
-let modal = document.querySelector('.modal');
-let buttonRequest = document.querySelector('.button-request');
+ const     recordButton = document.querySelector('#record-button'),
+         modal = document.querySelector('.modal'),
+         buttonRequest = document.querySelector('.button-request');
+
+  const  requestUserName = document.querySelector('.request-user-name'),
+         requestTel = document.querySelector('.request-tel'),
+         requestText = document.querySelector('.request-text');
 
 recordButton.addEventListener('click', function (event) {
     modal.classList.add('is-open');
@@ -9,10 +13,15 @@ recordButton.addEventListener('click', function (event) {
 
 buttonRequest.addEventListener('click', function (event) {
     if (form_is_correct()) {
-        modal.classList.remove('is-open');
+        modal.classList.remove('is-open')
     }
 });
 
+const  requestUserNameValue = requestUserName.value,
+    requestTelValue = requestTel.value,
+    requestTextValue = requestText.value;
+
+
 function form_is_correct() {
-    return true
+    return (requestUserNameValue !== '' && requestTelValue !=='' && requestTextValue !== '')
 }
